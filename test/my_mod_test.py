293a715584491916a2fca1054b2a29dba56e05de
Ping <pingao2019@outlook.com>
2020-04-09 16:36:16 -0400

    add test

---
+++ test/my_mod_test.py
@@ -0,0 +1,16 @@
+# test/my_mod_test.py
+
+import unittest
+from my_lambdata.my_mod import enlarge
+
+class TestMyMod(unittest.TestCase):
+
+    def test_enlarge(self):
+        """
+        Unit test that the enlarge function in the my_mod file is working properly
+        """
+        self.assertEqual(enlarge(5), 500)
+
+
+if __name__ == '__main__':
+    unittest.main()
